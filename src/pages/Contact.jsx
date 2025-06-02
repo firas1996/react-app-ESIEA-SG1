97f5c8b250e0e8f4bb5a8c1917803f70747072e5
firas1996 <firas.hm.1996@gmail.com>
2025-06-02 11:35:23 +0100

    aa

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Btn from "../components/Btn";
+import axios from "axios";
 
 const Contact = () => {
+  const [users, setUsers] = useState([]);
+  const getUsers = () => {
+    axios
+      .get("http://10.33.2.58:1122/users")
+      .then((res) => {
+        setUsers(res.data.data.users);
+        console.log(res.data.data.users);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   return (
     <div>
       <h1>Contact Page !!!</h1>
+      <button onClick={getUsers}>Get Users</button>
       <Btn />
     </div>
   );
