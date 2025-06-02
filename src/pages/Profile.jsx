2dd81e72bab7e0c51426831d5b3d4cbe687e5991
firas1996 <firas.hm.1996@gmail.com>
2025-06-02 11:08:01 +0100

    az

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Profile = () => {
+  const [c, setC] = useState(0);
+  const inc = () => {
+    setC(c + 1);
+    console.log(c);
+  };
+  const dec = () => {
+    setC(c - 1);
+    console.log(c);
+  };
   return (
     <div>
       <h1>Profile Page !!!</h1>
+      <button onClick={dec}>-</button>
+      <p>{c}</p>
+      <button onClick={inc}>+</button>
     </div>
   );
 };
